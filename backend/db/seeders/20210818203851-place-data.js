be91feb78fa0d65ae5f3c6c1acf3dcf692ef6c43
Nathan Darrow <nathanadarrow@gmail.com>
2021-08-18 14:15:18 -0700

    Fixing something

--- backend/db/seeders/20210818203851-place-data.js
+++ backend/db/seeders/20210818203851-place-data.js
@@ -2,12 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      */
       return queryInterface.bulkInsert('Places', [
         {
           title: 'Mos Eisley Cantina', city: 'Mos Eisley', state:'Great Mesra Plateau', imageUrl: 'https://starwarsblog.starwars.com/wp-content/uploads/2015/01/E4S_KEY_21.jpg',
@@ -15,7 +9,7 @@ module.exports = {
         },
         {
           title: 'Milliways: The Restaurant at the End of the Universe', city: 'End of the Universe', state: '???', imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1532522430i/26026311._SX540_.jpg',
-          description: 'Entertain your guests with what is truly the ultimate show! Warning: Guests with certain prophetic beliefs may be severely disappointed.'
+          description: 'Entertain your guests with what is quite literally the ultimate show! Warning: Guests with certain prophetic beliefs may be severely disappointed.'
         },
         {
           title: 'Gasworks', city: 'Aurora', state: 'Illinois', imageUrl: 'https://1.bp.blogspot.com/-aIrCOpeLpX8/Tf7V10vbQ3I/AAAAAAAAEkM/lKbqW3Qxsn0/s1600/Gas.JPG',
